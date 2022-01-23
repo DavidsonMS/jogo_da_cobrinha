417aa8d16d71b63cc9966afc48cf95ac984367f6
Davidson <davidsonmanzoli@gmail.com>
2022-01-23 19:27:02 -0300

    Definir aletoriedade da fruta e e crecismento da cobra

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let box = 32;
 let snake =[];
 snake[0] = {
     x: 8 * box,
-    y: 8 * box,
+    y: 8 * box
 }
 let direction = "right";
 let food = {
@@ -14,7 +14,7 @@ let food = {
 
 function CriarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 16*box, 16*box);
 }
 
 function criarCobrinha() {
@@ -40,11 +40,11 @@ function update(event) {
     
 }
 
-function iniciarJogo() {
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 * box && direction == "letf") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+function iniciarJogo(){
+       if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
 
 
 
@@ -62,7 +62,16 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop(); //fução que tira ultimo elemento do array
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //fução que tira ultimo elemento do array
+
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+
+    }
+    
 
     let newHead ={
         x: snakeX,
